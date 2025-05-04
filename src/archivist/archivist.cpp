1e4fbb7efdb3186f60cf4de0e64bf9dd18491da4
DIYLabsED <146977205+DIYLabsED@users.noreply.github.com>
2025-05-04 06:58:04 +0530

    Boilerplate for terminal interaction

---
+++ src/archivist/archivist.cpp
@@ -0,0 +1,33 @@
+/*
+    VATPlayback Archivist
+    Command line tool for recording VATSIM network traffic
+
+    (C) 2025 DIY Labs
+    Licensed under GNU GPL V3
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+void setup();
+
+
+int main(int argc, char * argv[]){
+
+    setup();
+
+    return 0;
+
+}
+
+
+void setup(){
+
+    std::cout << "VATPlayback Archivist\n"
+              << "Copyright (C) 2025 DIY Labs\n"
+              << "Licensed under GNU GPL V3.\n";
+
+}
